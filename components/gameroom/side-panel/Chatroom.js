8a6ba6eedf3b64c807a9e7d5788c3e0fe25b2981
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-05-05 23:37:21 -0400

    Chat feature with socket.io

--- components/gameroom/side-panel/Chatroom.js
+++ components/gameroom/side-panel/Chatroom.js
@@ -1,12 +1,84 @@
 import gameRoomStyles from '../../../styles/GameRoom.module.css'
+import chatRoomStyles from '../../../styles/Chatroom.module.css'
+
+
+import { useRouter } from 'next/router'
+import { useEffect, useRef, useState } from "react";
+import socketIOClient from "socket.io-client";
+
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage"; // Name of the event
+const SOCKET_SERVER_URL = "http://localhost:4000";
+
 
 const Chatroom = () => {
-    return(
-        <section className={gameRoomStyles.innerSidePanelContainer}>
-            <div>
-                <p>Container for the chat feature to go in</p>
-                {/* <textarea placeholder="type your message here..."></textarea> */}
+
+    const router = useRouter()
+    const { roomId } = router.query; // Gets roomId from URL
+    // const { messages, sendMessage } = useChat(roomId); // Creates a websocket and manages messaging
+    const [messages, setMessages] = useState([]); // Sent and received messages
+    const socketRef = useRef();
+
+    const [newMessage, setNewMessage] = useState(""); // Message to be sent
+
+    const handleNewMessageChange = (event) => {
+        setNewMessage(event.target.value);
+    };
+
+    const handleSendMessage = () => {
+        socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+            body: newMessage,
+            senderId: socketRef.current.id,
+        });
+        setNewMessage("");
+    };
+
+    useEffect(() => {
+
+        // Creates a WebSocket connection
+        socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+            query: { roomId },
+        });
+
+        // Listens for incoming messages
+        socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (message) => {
+            const incomingMessage = {
+                ...message,
+                ownedByCurrentUser: message.senderId === socketRef.current.id,  // check if message was sent by current user
+            };
+            setMessages((messages) => [...messages, incomingMessage]);
+        });
+
+        // Destroys the socket reference
+        // when the connection is closed
+        return () => {
+            socketRef.current.disconnect();
+        };
+    }, [roomId]);
+    return (
+        // <section className={gameRoomStyles.innerSidePanelContainer}></section>
+        <section>
+            <div className={chatRoomStyles.messagesContainer}>
+                <ul className={chatRoomStyles.messagesList}>
+                    {messages.map((message, i) => (
+                        <li
+                            key={i}
+                            className={`${message.ownedByCurrentUser ? chatRoomStyles.myMessage : chatRoomStyles.receivedMessage
+                                }`}
+                        >
+                            {message.body}
+                        </li>
+                    ))}
+                </ul>
             </div>
+            <textarea
+                value={newMessage}
+                onChange={handleNewMessageChange}
+                placeholder="Type your message here..."
+                className={chatRoomStyles.newMessagesTextArea}                
+            />
+            <button onClick={handleSendMessage} className={chatRoomStyles.sendMessageButton}>
+                SEND
+            </button>
         </section>
     )
 }
