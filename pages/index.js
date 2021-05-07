b8182b0f205511e3ba525e44ad2a3ce7a50b8d63
Hustin <hustinkava@gmail.com>
2021-05-06 19:15:45 -0600

    layout

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Party House</title>
       </Head>
-      <div class="container mx-auto">
+      <div className="container mx-auto">
         <div className="grid grid-cols-2 gap-20">
           <div className={styles.left}>
             <h1 className={styles.h1}>Party House</h1>
@@ -17,7 +17,28 @@ export default function Home() {
 
           <div className={styles.outer}>
             <img className={styles.below} src='./assets/images/homepage/cardstack.png' alt='card stack'/>
-            <img className={styles.top} src='./assets/images/homepage/card.png' alt='card stack'/>
+
+            {/* <img src='./assets/images/homepage/cardtest.png' alt='card stack'/> */}
+            
+            <div className={styles.top}>
+
+
+              <div className={styles.buttons}>
+                <div className="grid grid-cols-1 gap-10">
+                  <button className="bg-gray-900 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
+                    SIGN UP
+                  </button>
+                  <button className="bg-purple-300 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
+                    LOGIN
+                  </button>
+                  <button className="bg-purple-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
+                    GUEST
+                  </button>
+                </div>
+              </div>
+
+            </div>
+
           </div>
         </div>
       </div>
