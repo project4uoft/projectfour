90b6e313d8130ad9a7351160934ac560fa17b6f6
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-05-08 09:57:20 -0400

    oAuth merged

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import Layout from "../components/Layout";
 // import "../styles/globals.css";
 import 'tailwindcss/tailwind.css'
+import { UserProvider } from '@auth0/nextjs-auth0';
+
 
 
 function MyApp({ Component, pageProps }) {
   return (
+    <UserProvider>
+
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </UserProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Head from "next/head";
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
-  return (
-    <div>
-      <h1>
-        Home Page
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div>
+        <h1>
+          Home Page
       </h1>
 
-    </div>
-  );
-}
+      </div>
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
+};
