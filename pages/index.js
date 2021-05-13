2630434a62e933a27f8b76b9a1c56c6e78ec409b
Hustin <hustinkava@gmail.com>
2021-05-12 18:48:03 -0600

    homepage refactor

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { v4 as uuid } from "uuid";
-import { Container, Grid } from '@material-ui/core';
+import { Container, Grid, Button } from '@material-ui/core';
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
@@ -23,98 +23,96 @@ export default function Home() {
       <Head>
         <title>Party House</title>
       </Head>
-      <Container maxWidth="sm">
 
-          {/* Column1 */}
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <h1 className='test1'>Party House</h1>
-              <img
-                className={styles.logo}
-                src="./assets/images/homepage/Logo1.png"
-                alt="Logo"
-              />
-              <h3 className='test2'>
+      <Container maxWidth="lg" className={styles.container}>
+        <Grid container spacing={6}>
+
+            {/* Column1 */}
+            <Grid item xs={12} sm={6} md={6} lg={6}>
+              <h1 className={styles.h1}>Party House</h1>
+              <img className={styles.logo} src="./assets/images/homepage/Logo1.png" alt="Logo"/>
+              <h3 className={styles.h3}>
                 Play games with friends from your home
               </h3>
             </Grid>
 
-            <Grid item xs={6}>
-              {/* Column2 */}
-              <div className={styles.outer}>
-                <img
-                  className={styles.below}
-                  src="./assets/images/homepage/cardstack.png"
-                  alt="card stack"
-                />
+            {/* Column2 */}
+            <Grid item xs={12} sm={6} md={6} lg={6}>
 
-                {/* <img src='./assets/images/homepage/cardtest.png' alt='card stack'/> */}
+
+
+              <div className={styles.outer}>
+                <img className={styles.below} src='./assets/images/homepage/cardstack.png' alt='card stack' />
+
+            {/* <img src='./assets/images/homepage/cardtest.png' alt='card stack'/> */}
 
                 <div className={styles.top}>
-                  
-                  {isLoading && <p>Loading login info...</p>}
+              
+                {isLoading && <p>Loading login info...</p>}
+                  {error && (
+                    <>
+                      <h4>Error</h4>
+                      <pre>{error.message}</pre>
+                    </>
+                  )}
+                <div className={styles.buttons}>
+                <div className="grid grid-cols-1 gap-10">
+                  {!user ? (
+                    <>
 
-                        {error && (
-                          <>
-                            <h4>Error</h4>
-                            <pre>{error.message}</pre>
-                          </>
-                        )}
+                    <div className={styles.vertical}>
+                    <Grid container spacing={6}>
+                      <Grid item xs={12} sm={12} md={12} lg={12}>
 
-                  <div className={styles.buttons}>
-                    <div className="grid grid-cols-1 gap-10">
-                      {!user ? (
-                        <>
-                          {/* <form action="/api/auth/login" className="px-4 py-2 rounded ">
-                            <input type="submit" className="font-bold text-white bg-gray-900 hover:bg-blue-900" value="Sign Up or Login" />
-                          </form> */}
-                          <Link href="/api/auth/login">
-                            <button className="flex justify-center rounded hover:bg-grey">
-                              <img
-                                src=".././assets/images/homepage/SignupButton.png"
-                                alt="login"
-                              />
-                            </button>
-                          </Link>
-                          <Link href="/api/auth/login">
-                            <button className="flex justify-center rounded hover:bg-grey">
-                              <img
-                                src=".././assets/images/homepage/LoginButton.png"
-                                alt="login"
-                              />
-                            </button>
-                          </Link>
-                          <Link href="/api/auth/login">
-                            <button className="flex justify-center rounded hover:bg-grey">
-                              <img
-                                src=".././assets/images/homepage/GuestButton.png"
-                                alt="Guest"
-                              />
-                            </button>
-                          </Link>
-                        </>
-                      ) : (
-                        <>
-                          <Link href="/api/auth/logout">
-                            <button className="flex justify-center rounded hover:bg-grey">
-                              <img
-                                src=".././assets/images/homepage/GuestButton.png"
-                                alt="Guest"
-                              />
-                            </button>
-                          </Link>
-                        </>
-                        )}
-                      </div>
+                      <Link href="/api/auth/login">
+                        <Button variant="contained" color="primary" variant="contained" fullWidth className={styles.button1}>
+                          <span className={styles.font}>SIGN UP</span>
+                        </Button>
+                      </Link>
+
+                      </Grid>
+
+                      <Grid item xs={12} sm={12} md={12} lg={12}>
+
+                      <Link href="/api/auth/login">
+                        <Button variant="contained" color="primary" variant="contained" fullWidth className={styles.button2}>
+                          <span className={styles.font}>LOGIN</span>
+                        </Button>
+                      </Link>
+
+                      </Grid>
+
+                      <Grid item xs={12} sm={12} md={12} lg={12}>
+
+                      <Link href="/api/auth/login">
+                        <Button variant="contained" color="primary" variant="contained" fullWidth className={styles.button3}>
+                          <span className={styles.font}>GUEST</span>
+                        </Button>
+                      </Link>
+
+                      </Grid>
+                    </Grid>
                     </div>
+
+                    </>
+                  ) : (
+
+
+                    <>
+                      <Link href="/api/auth/logout">
+                        <button className="flex justify-center rounded hover:bg-grey">
+                          <img src=".././assets/images/homepage/GuestButton.png" alt="Guest"/>
+                        </button>
+                      </Link>
+                    </>
+                  )}
                   </div>
                 </div>
-            </Grid>
+              </div>
+            </div>
 
           </Grid>
-
-
-
+        </Grid>
       </Container>
     </div>
   );
