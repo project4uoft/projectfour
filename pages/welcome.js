fbed042d75a3d06a3c72c1e629e34cc9466a44bd
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-05-13 20:26:38 -0400

    Added Welcome Page with card animation

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,17 +1,62 @@
 import Head from "next/head";
 import Navbar from '../components/navbar/navbar';
-import styles from '../styles/Welcome.module.css';
+import Card from "../components/WelcomeCard";
+import { Container, Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 
 export default function Welcome() {
 
-  return (
+    return (
 
-    <div>
-        <Head>
-            <title>Welcome</title>
-        </Head>
-        <Navbar/>
-    </div>
+        <>
+            <Head>
+                <title>Welcome</title>
+            </Head>
+            <Navbar />
 
-  );
+            <Container maxWidth="lg">
+                <Grid container spacing={4}
+                >
+                    {/* Column1 - Form */}
+                    <Grid item xs={12} spacing={4}>
+                        <form noValidate autoComplete="off" spacing={2}>
+                            <TextField id="filled-basic" label="Enter Room ID" variant="filled" fullWidth={true} />
+                            <Button color="secondary" variant="outlined" fullWidth={true}>Join</Button>
+                        </form>
+                    </Grid>
+                    {/* Column3 */}
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                        <Card />
+                    </Grid>
+                    {/* Column3 */}
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                        <Card />
+                    </Grid>
+                    {/* Column4 */}
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                        <Card />
+                    </Grid>
+                    {/* Column5 */}
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                        <Card />
+                    </Grid>
+                    {/* Column6 */}
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                        <Card />
+                    </Grid>
+                    {/* Column7 */}
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                        <Card />
+                    </Grid>
+
+                </Grid>
+            </Container>
+
+
+
+        </>
+
+    );
 }
